fb06b0c55e6a9d8fcb363e2955011313550f0812
Chivonne Reji <chivonne.reji@olx.com>
2021-02-01 22:07:33 +0530

    Initial commit for sample pod

---
+++ SamplePod/SamplePod.h
@@ -0,0 +1,19 @@
+//
+//  SamplePod.h
+//  SamplePod
+//
+//  Created by OLX on 01/02/21.
+//  Copyright © 2021 OLX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SamplePod.
+FOUNDATION_EXPORT double SamplePodVersionNumber;
+
+//! Project version string for SamplePod.
+FOUNDATION_EXPORT const unsigned char SamplePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SamplePod/PublicHeader.h>
+
+
---
+++ SamplePodApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SamplePodApp
+//
+//  Created by OLX on 01/02/21.
+//  Copyright © 2021 OLX. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
